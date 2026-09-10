903b9bc94c522b1dff9f4aab4f14f50ca5e6f72f
cvcv89 <125327331+cvcv89@users.noreply.github.com>
2026-09-10 11:35:48 -0500

    added player controls

--- Assets/Scrpits/NewMonoBehaviourScript.cs
+++ Assets/Scrpits/NewMonoBehaviourScript.cs
@@ -1,17 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     float speed = 15f;
+    float Turnspeed = 10f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Turnspeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Turnspeed);
+        }
+            
+
     }
 }
